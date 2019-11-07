2f723981e02ae48968bec8132bf83d1e44495e4c
Eduardo Silva <edlusilvaca@ittepic.edu.mx>
2019-11-06 17:01:54 -0700

    side menu con enlace a paginas

---
+++ src/app/dia/dia.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { DiaPage } from './dia.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DiaPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [DiaPage]
+})
+export class DiaPageModule {}
